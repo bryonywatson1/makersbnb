e9376ec448514ba57ddff17769a5baae196c2ad9
ajbater <amanda22@hotmail.co.uk>
2016-12-12 18:16:32 +0000

    user cannot sign in with incorrect credentials

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,4 +7,9 @@ feature "User Sign in" do
     sign_in(email: "example@email.com", password: "1234")
     expect(page).to have_content "Spaces"
   end
+
+  scenario "with incorrect credentials" do
+    sign_in(email: "example@email.com", password: "5678")
+    expect(page).to have_content "The email or password is incorrect"
+  end
 end
