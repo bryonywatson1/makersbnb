7f9e81d5a619d5d25112dcbb2a535de449931cb5
keomony khun <keomony@gmail.com>
2016-12-12 19:29:57 +0000

    user can sign out, adds layout

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,4 +1,4 @@
-feature "User Sign in" do
+feature "User sign in" do
   before do
     sign_up(email: "example@email.com", password: "1234", password_confirmation: "1234")
   end
@@ -7,4 +7,10 @@ feature "User Sign in" do
     sign_in(email: "example@email.com", password: "1234")
     expect(page).to have_content "Spaces"
   end
+
+  scenario "with incorrect credentials" do
+   sign_in(email: "example@email.com", password: "5678")
+   expect(page).to have_content "The email or password is incorrect"
+ end
+
 end
