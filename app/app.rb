f346667ba8ae78729c752f271b20091b00bd8294
Bryony Watson <bryonywatson1@googlemail.com>
2016-12-12 16:28:12 +0000

    changed name of properties file to spaces and set up data mapper for space rb

--- app/app.rb
+++ app/app.rb
@@ -7,7 +7,7 @@ require 'sinatra/partial'
 require_relative 'data_mapper_setup'
 
 require_relative 'controllers/users'
-require_relative 'controllers/properties'
+require_relative 'controllers/spaces'
 
 
 class MakersBnb < Sinatra::Base
