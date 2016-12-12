ac7e9d913c5b1e2fc0cefe6e41958e4110b0025d
ajbater <amanda22@hotmail.co.uk>
2016-12-12 16:29:33 +0000

    updates data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,3 +1,5 @@
+require 'dm-validations'
+require 'dm-migrations'
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-timestamps'
@@ -6,4 +8,3 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
