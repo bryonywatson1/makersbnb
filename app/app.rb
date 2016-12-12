7db909fa408797da44619e16457f118e86796d47
Chris Cooper <cjcooper252@gmail.com>
2016-12-12 15:26:22 +0000

    sets up directories

---
+++ app/app.rb
@@ -0,0 +1,30 @@
+ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'sinatra/partial'
+
+require_relative 'data_mapper_setup'
+
+require_relative 'controllers/users'
+require_relative 'controllers/properties'
+
+
+class MakersBnb < Sinatra::Base
+
+  enable :sessions
+  register Sinatra::Flash
+  register Sinatra::Partial
+  use Rack::MethodOverride
+  set :session_secret, 'super secret'
+  set :root, File.dirname(__FILE__)
+  set :partial_template_engine, :erb
+
+  enable :partial_underscores
+
+  get '/' do
+    'Hello MakersBnb!'
+  end
+
+  run! if app_file == $0
+end
