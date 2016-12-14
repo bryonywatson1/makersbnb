b2a6522ef662e846d2bafdfbf6dc428720fb2b16
keomony khun <keomony@gmail.com>
2016-12-14 21:37:01 +0000

    validates all fields of add new space form

--- app/public/js/validation.js
+++ app/public/js/validation.js
@@ -1,40 +1,12 @@
 
 "use strict";
 
-
-  $( function() {
-    $( "#available_from" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
-  } );
-
-  $( function() {
-    $("#available_to" ).datepicker({dateFormat: "yy-mm-dd",  minDate: 1});
-  } );
-
-
-
 $(document).ready(function(){
-    $("#adds_new_space").submit(function(){
-      var fromDate = $("#available_from").val();
-      var toDate = $("#available_to").val();
-      if(fromDate >= toDate){
-        alert("To date must be after From date");
-        return false;
-      }
+    var today = new Date().toISOString().split('T')[0];
+    $("#available_from").attr('min', today);
+
+    $("#available_from, #available_to").on("change", function(){
+      $("#available_to").attr("min", $("#available_from").val());
     });
+
 });
-
-
-
-
-
-
-
-
-
-$( function() {
-    $("#requested_date" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
-  } );
-
-  $( function() {
-      $("#filter_date" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
-    } );
