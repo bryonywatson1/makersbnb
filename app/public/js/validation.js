b632021030aa27dbe36c836723574810400840db
keomony khun <keomony@gmail.com>
2016-12-14 22:00:15 +0000

    changes all input types of calendar from text to date

--- app/public/js/validation.js
+++ app/public/js/validation.js
@@ -2,11 +2,14 @@
 "use strict";
 
 $(document).ready(function(){
-    var today = new Date().toISOString().split('T')[0];
-    $("#available_from").attr('min', today);
+    var today = new Date().toISOString().split("T")[0];
+    $("#available_from").attr("min", today);
 
     $("#available_from, #available_to").on("change", function(){
       $("#available_to").attr("min", $("#available_from").val());
     });
+
+    $("#filter_date").attr("min", today);
+    $("#requested_date").attr("min", today);
 
 });
