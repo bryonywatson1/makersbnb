f626a4504d07bb8343dac5b938968de5352296bb
Bryony Watson <bryonywatson1@googlemail.com>
2016-12-14 16:00:18 +0000

    added proper calendars and got rid of errors

--- app/public/js/validation.js
+++ app/public/js/validation.js
@@ -1,13 +1,30 @@
 
 "use strict";
 
+
   $( function() {
     $( "#available_from" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
   } );
 
   $( function() {
-    $("#available_to" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
+    $("#available_to" ).datepicker({dateFormat: "yy-mm-dd",  minDate: 1});
   } );
+
+
+
+$(document).ready(function(){
+    $("#adds_new_space").submit(function(){
+        alert(startDate);
+    });
+});
+
+
+
+
+
+
+
+
 
 $( function() {
     $("#requested_date" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
