ce9f95c3c2b988d9c4a3f4b50f1dd6adb8d6bc18
Bryony Watson <bryonywatson1@googlemail.com>
2016-12-14 15:12:27 +0000

    made jquery calendars for all date fields

--- app/public/js/validation.js
+++ app/public/js/validation.js
@@ -2,9 +2,17 @@
 "use strict";
 
   $( function() {
-    $( "#available_from" ).datepicker({ minDate: 0 });
+    $( "#available_from" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
   } );
 
   $( function() {
-    $("#available_to" ).datepicker({ minDate: 0 });
+    $("#available_to" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
   } );
+
+$( function() {
+    $("#requested_date" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
+  } );
+
+  $( function() {
+      $("#filter_date" ).datepicker({ minDate: 0 , dateFormat: "yy-mm-dd"});
+    } );
