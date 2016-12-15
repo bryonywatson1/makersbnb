5abf393de55b0ad634a182d5321b1abbd03ecb7f
keomony khun <keomony@gmail.com>
2016-12-15 14:33:10 +0000

    changes date format for requested_date to yy-mm-dd

--- app/public/js/validation.js
+++ app/public/js/validation.js
@@ -2,10 +2,6 @@
 "use strict";
 
 
-
-
-
-
 $(document).ready(function(){
 
     $("#available_from").datepicker({
@@ -21,34 +17,30 @@ $(document).ready(function(){
       dateFormat: "yy-mm-dd"
     });
 
-    // $("#requested_date").datepicker({
-    //   minDate: 0,
-    //   dateFormat: "yy-mm-dd"
-    //   // beforeShowDay: check_available_date(available_formatted_dates_list)
-    // });
-
     $("#available_from, #available_to").on("change", function(){
       var x=$("#available_from").datepicker("getDate");
       $( "#available_to" ).datepicker( "option", "minDate",x );
     });
 
     var availableDates = [];
-    // fill the available dates list from the hidden fields
     $("#sending_request input[type=hidden]").each(function() {
          availableDates.push($(this).val());
     });
-    //var availableDates = ["2017-5-9","2017-5-14","2017-5-15"];
 
     function available(date) {
-      var dmy = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-      if ($.inArray(dmy, availableDates) != -1) {
+      var ymd = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+      if ($.inArray(ymd, availableDates) != -1) {
         return [true, "","Available"];
       } else {
         return [false,"","unAvailable"];
       }
   }
 
-$('#requested_date').datepicker({ beforeShowDay: available });
+    $('#requested_date').datepicker({
+      beforeShowDay: available
+    });
+
+    $('#requested_date').datepicker("option","dateFormat","yy-mm-dd");
 
 
 
